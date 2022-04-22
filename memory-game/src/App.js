45c6ba83f80bbe2884346b5b8c55db8024a5b218
Avery Hampton <averydhampton@icloud.com>
2022-04-22 09:49:50 -0500

    completes part 1

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
+import MemoryCard from "./components/MemoryCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <title>Memory Game</title>
         <h1 className="Game-title">Memory Game</h1>
         <h3 className="Game-subtitle">Match Cards To Win</h3>
       </header>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
     </div>
   );
 }
